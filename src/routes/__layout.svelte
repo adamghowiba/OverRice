2aed9a5e860bad7eb35462979ee4feb22850bb29
Nathan <39814328+Yofou@users.noreply.github.com>
2021-08-23 20:01:59 +0100

    [📦] basic sass

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<slot />
+
+<style global lang="scss">
+    // Base
+    @use '../lib/scss/2-base/reset';
+    @use '../lib/scss/2-base/global';
+    @use '../lib/scss/2-base/typography';
+</style>
---
+++ src/routes/foo.svelte
@@ -0,0 +1,10 @@
+<script lang="typescript" context="module">
+    import type { Load } from "@sveltejs/kit";
+
+    export const load: Load = () => {
+        return {
+            status: 301,
+            redirect: '/contact',
+        }
+    }
+</script>
