261a5503991e8745bcc40a541d281bd2aa5c8549
Adam <adamware99@hotmail.com>
2022-01-19 08:39:52 +0400

    Added changes to fixes

--- src/routes/location/_components/Map.svelte
+++ src/routes/location/_components/Map.svelte
@@ -1,89 +1,93 @@
 <script context="module">
-    const map_access_token = 'pk.eyJ1Ijoid2VicmV2aXZlZCIsImEiOiJja3Q5djB4N3ExZm5lMndwbG9mYmw5b28zIn0.tMGZEaDUKymaWr0D28gnVA'
+	const map_access_token =
+		'pk.eyJ1Ijoid2VicmV2aXZlZCIsImEiOiJja3Q5djB4N3ExZm5lMndwbG9mYmw5b28zIn0.tMGZEaDUKymaWr0D28gnVA';
 </script>
 
 <script lang="ts">
-    import { afterUpdate, getContext, setContext } from 'svelte';
-    import type { Writable } from 'svelte/store';
+	import { afterUpdate, getContext, setContext } from 'svelte';
+	import type { Writable } from 'svelte/store';
 
-    export let address: string
-    export let showPage: Writable<boolean>
+	export let address: string;
+	export let showPage: Writable<boolean>;
 
-    let map;
-    let leaflet;
-    let markers = [];
-    let iconMarker; 
-    afterUpdate ( async () => {
-        // we have to import leaflet this way, as leaflet uses they window object
-        // on import, which is only available after we mounted.
-        try {
-            if (!leaflet) {
-                leaflet = await import( 'leaflet/dist/leaflet' )
-                
-                iconMarker = leaflet.icon({
-                    iconUrl: '/icons/Marker.png',
-                    iconSize: [80, 80]
-                })
-            }
-            
-            let longlat = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${map_access_token}&limit=1`)
-                .then( res => res.json() )
-                .then( coords => {
-                    const coord = coords.features[0]
-                    return [coord.center[1], coord.center[0]]
-                })
-    
-            if (!map) map = leaflet.map("map") 
-            map.setView(longlat, 13);
-            map.setZoom(17)
-            
-            leaflet.tileLayer(
-                'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
-                {
-                    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                    id: 'mapbox/streets-v11',
-                    accessToken: map_access_token,
-                    tileSize: 512,
-                    zoomOffset: -1,
-                    maxZoom: 18,
-                }
-            )
-            .addTo(map)
-            
-            // removes previous markers
-            if (markers.length >= 1) {
-                markers.forEach( marker => map.removeLayer( marker ) )
-                markers = []
-            }
-    
-            markers.push( leaflet.marker(longlat, { icon: iconMarker }) )
-            markers.push( leaflet.circle(longlat, { 
-                radius: 50, 
-                color: '#9DC471', 
-                fill: true, 
-                fillColor: '#FFFFFF', 
-                fillOpacity: 0.3 
-            }) )
-            markers.forEach( marker => marker.addTo(map) );
-        } catch (e) {
-            console.error(e)
-            showPage.set(false)
-        }
-    } )
+	let map;
+	let leaflet;
+	let markers = [];
+	let iconMarker;
+	// afterUpdate ( async () => {
+	//     // we have to import leaflet this way, as leaflet uses they window object
+	//     // on import, which is only available after we mounted.
+	//     try {
+	//         if (!leaflet) {
+	//             leaflet = await import( 'leaflet/dist/leaflet' )
+
+	//             iconMarker = leaflet.icon({
+	//                 iconUrl: '/icons/Marker.png',
+	//                 iconSize: [80, 80]
+	//             })
+	//         }
+
+	//         let longlat = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=${map_access_token}&limit=1`)
+	//             .then( res => res.json() )
+	//             .then( coords => {
+	//                 const coord = coords.features[0]
+	//                 return [coord.center[1], coord.center[0]]
+	//             })
+
+	//         if (!map) map = leaflet.map("map")
+	//         map.setView(longlat, 13);
+	//         map.setZoom(17)
+
+	//         leaflet.tileLayer(
+	//             'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+	//             {
+	//                 attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	//                 id: 'mapbox/streets-v11',
+	//                 accessToken: map_access_token,
+	//                 tileSize: 512,
+	//                 zoomOffset: -1,
+	//                 maxZoom: 18,
+	//             }
+	//         )
+	//         .addTo(map)
+
+	//         // removes previous markers
+	//         if (markers.length >= 1) {
+	//             markers.forEach( marker => map.removeLayer( marker ) )
+	//             markers = []
+	//         }
+
+	//         markers.push( leaflet.marker(longlat, { icon: iconMarker }) )
+	//         markers.push( leaflet.circle(longlat, {
+	//             radius: 50,
+	//             color: '#9DC471',
+	//             fill: true,
+	//             fillColor: '#FFFFFF',
+	//             fillOpacity: 0.3
+	//         }) )
+	//         markers.forEach( marker => marker.addTo(map) );
+	//     } catch (e) {
+	//         console.error(e)
+	//         showPage.set(false)
+	//     }
+	// } )
 </script>
 
 <svelte:head>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-    crossorigin=""/>
+	<link
+		rel="stylesheet"
+		href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+		integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+		crossorigin=""
+	/>
 </svelte:head>
 
 <div class="map">
-    <div class="map__header">
-        <img src="/icons/map_location.svg" alt="">
-        <h1>{address}</h1>
-    </div>
-    <div id="map" />
+	<div class="map__header">
+		<img src="/icons/map_location.svg" alt="" />
+		<h1>{address}</h1>
+	</div>
+	<div id="map" />
 </div>
 
 <style lang="scss">
