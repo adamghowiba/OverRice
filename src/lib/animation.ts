2d9f1cff773f4a0e2ac263a374a4079ba881c667
Adam <adamware99@hotmail.com>
2022-03-04 23:13:52 +0400

    Added animation changes to homepage, added review controls

--- src/lib/animation.ts
+++ src/lib/animation.ts
@@ -5,13 +5,13 @@ export const setupHeaderAnimation = () => {
 
   timeline.set('.bottom__bar__item', { opacity: 0 });
   timeline.add(
-    gsap.from('.hero__headings__h1', { delay: 0.2, opacity: 0, duration: 0.5, ease: 'power1' })
+    gsap.from('.hero__headings__h1', { delay: 0.2, opacity: 0, yPercent: -5, duration: 0.5, ease: 'power1' })
   );
   timeline.add(
     gsap.from('.hero__headings__p, .hero__headings__button-wrap, .subtitle--hero', {
       delay: 0.3,
       opacity: 0,
-      yPercent: 100,
+      xPercent: -10,
       duration: 0.5,
       ease: 'power1',
       stagger: 0.4,
@@ -23,7 +23,7 @@ export const setupHeaderAnimation = () => {
 export const setupQuickAction = () => {
   let timeline = gsap.timeline();
   timeline.add(gsap.set('.bottom__bar__item', { opacity: 1 }));
-  timeline.add(gsap.from('.bottom__bar__item', { opacity: 0, y: '-=20', stagger: 0.4 }));
+  timeline.add(gsap.from('.bottom__bar__item', { opacity: 0, stagger: 0.2 }));
 
   return timeline;
 };
--- src/lib/home/Review.svelte
+++ src/lib/home/Review.svelte
@@ -1,66 +1,122 @@
-<script>
-  import { onDestroy, onMount } from "svelte";
-  import { fade } from "svelte/transition";
+<script lang="ts">
+  import { onDestroy, onMount } from 'svelte';
+  import { fade } from 'svelte/transition';
 
-  const REVIEWS = [
-    "First time eating from OverRice today!!! WOW!!! DELICIOUS!!! Definitely will be back again. I had the Huli huli chicken. It came with rice, pancit, and lumpia. The chicken was moist and flavorful, the rice was hot and fluffy,",
-    "I'm always a little put off by food truck food because I always assume portions are small. Over Rice proved me wrong! I did get the two meat option, but it was enough to share with someone else, & we stayed full for a couple of hours.",
-    "Extremely good. I had the chicken with noodles and rice. I wish I could buy their egg rolls by the dozen.",
-    "I am so happy to have found OverRice at the Lake Eola Farmer's Market, because my wife and I were looking for something delicious; but, different; and, OverRice delivered!",
+  interface Reviews {
+    review: string;
+    name: string;
+  }
+
+  const REVIEWS: Reviews[] = [
+    {
+      review:
+        'First time eating from OverRice today!!! WOW!!! DELICIOUS!!! Definitely will be back again. I had the Huli huli chicken. It came with rice, pancit, and lumpia. The chicken was moist and flavorful, the rice was hot and fluffy,',
+      name: 'Leilani B.',
+    },
+    {
+      review:
+        "I'm always a little put off by food truck food because I always assume portions are small. Over Rice proved me wrong! I did get the two meat option, but it was enough to share with someone else, & we stayed full for a couple of hours.",
+      name: 'Alexis T.',
+    },
+    {
+      review:
+        "The first time I tried OverRice was at the downtown farmer's market at Lake Eola. The staff was super nice and friendly. I got the pork adobo meal and it was delicious- juicy meat, awesome flavor, and nice crispy lumpia rolls.",
+      name: 'Lisa S.',
+    },
+    {
+      review:
+        "It's about time I share this truck with the world! My sister introduced me to the Over Rice truck many years ago and even though she's since moved to Chicago this is ALWAYS on the hit list when she comes home.",
+      name: 'Sasha S.',
+    },
   ];
 
-  let currentReview = REVIEWS[0];
-  let intervalID;
   let reviewIndex = 0;
+  $: currentReview = REVIEWS[0];
+  let intervalID;
 
-  function resizeHeight() {
-
+  function changeReview(index: number) {
+    if (index >= REVIEWS.length) index = 0;
+    
+    reviewIndex = index;
+    currentReview = REVIEWS[index];
   }
 
-  function changeReview(max = REVIEWS.length) {
-    if (reviewIndex >= max) reviewIndex = 0;
+  const handleControlClick = (event: MouseEvent) => {
+    const target = event.target as HTMLElement;
 
-    currentReview = REVIEWS[reviewIndex];
+    const controlId = parseInt(target.dataset.id);
+    changeReview(controlId);
+  };
 
-    reviewIndex += 1;
-  }
+  const onMouseEnter = () => {
+    if (!intervalID) return;
+    clearInterval(intervalID);
+  };
+
+  const onMouseLeave = () => {
+    if (!intervalID) return;
+    startReviewCycle();
+  };
+
+  const startReviewCycle = () => {
+    intervalID = setInterval(() => {
+      changeReview(++reviewIndex);
+    }, 3500);
+  };
 
   onMount(() => {
-    intervalID = setInterval(() => {
-      changeReview();
-    }, 2500);
+    startReviewCycle();
 
-  });
-
-  onDestroy(() => {
-    if (intervalID) return clearInterval(intervalID);
+    return () => {
+      if (intervalID) return clearInterval(intervalID);
+    };
   });
 </script>
 
 <!-- Review Section -->
-<div class="review-wrap">
+<div class="review-wrap" on:mouseleave={onMouseLeave} on:mouseenter={onMouseEnter}>
   <!-- <img class="review__image" src="{image}" alt="Chicken kabab"/> -->
 
   <div class="review">
     <div class="review__content-wrap">
-      <img
-        class="review__icon"
-        src="/icons/quote.svg"
-        alt="Green  quote icon"
-      />
+      <img class="review__icon" src="/icons/quote.svg" alt="Green  quote icon" />
       <!-- TODO Add Slider -->
       {#key currentReview}
         <p in:fade={{ duration: 500 }}>
-          {currentReview}
+          {currentReview.review}
         </p>
+        <span class="review__name">{currentReview.name}</span>
       {/key}
-      <span class="review__name">Jeff W.</span>
+    </div>
+    <div class="controls">
+      {#each REVIEWS as _, i}
+        <div class="controls__circle" data-id={i} class:active={i === reviewIndex} on:click={handleControlClick} />
+      {/each}
     </div>
   </div>
 </div>
 
 <style lang="scss">
   @use '../scss/0-helpers/vars' as *;
+
+  .controls {
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
+
+    &__circle {
+      margin-top: 20px;
+      width: 40px;
+      height: 11px;
+      border: 1px solid rgba(255, 255, 255, 0.5);
+      border-radius: 3px;
+    }
+
+    .active {
+      background-color: $color-green;
+      border: 1px solid $color-green;
+    }
+  }
 
   .review {
     max-height: 300px;
@@ -98,11 +154,9 @@
     height: 100%;
     padding: clamp(2rem, 6%, 5rem);
 
-    background: linear-gradient(rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)),
-      var(--review);
+    background: linear-gradient(rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.65)), var(--review);
     background-repeat: no-repeat;
     background-size: cover;
     background-position: var(--bg-pos, center);
-
   }
 </style>
