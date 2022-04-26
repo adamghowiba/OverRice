a1eb1cb7ea6c9e09f4597630b2b1ab9ef0ad72f4
Adam <adamware99@hotmail.com>
2022-04-27 02:24:43 +0400

    fix: change to adapter-node

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,8 +9,6 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-		// trailingSlash: 'ignore',
-		// hydrate the <div id="svelte"> element in src/app.html
 	}
 };
 
