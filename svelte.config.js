e85da9dab2edb5365ff8201df009d9de061387b7
Adam <adamware99@hotmail.com>
2022-10-21 04:03:55 +0400

    Attempt to fix deployment

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,6 @@ import preprocess from 'svelte-preprocess';
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-
   preprocess: preprocess(),
   kit: {
     adapter: adapter(),
